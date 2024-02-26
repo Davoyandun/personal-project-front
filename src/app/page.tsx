340cb68587d0a780c37386a78ce528754c0a1580
David Yandún <yandundavid@gmail.com>
2024-02-25 23:08:00 -0500

    feat[final-3]: implement background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { ModeToggle } from "@/components/theme/buttonToggleDark";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+    <main className="flex min-h-screen flex-col items-center justify-between p-20 w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative  ">
+     
+     <NavbarComponent />
       <InfiniteGallery />
       <ModeToggle />
-      <NavbarComponent />
+     
     </main>
   );
 }
